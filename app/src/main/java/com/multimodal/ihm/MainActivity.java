fc24028f92dd72773b4a34dd29579ce8aa1de993
Th3CracKed <abdodriowya@gmail.com>
2019-11-23 19:33:02 +0100

    add cursor

--- app/src/main/java/com/multimodal/ihm/MainActivity.java
+++ app/src/main/java/com/multimodal/ihm/MainActivity.java
@@ -1,37 +1,315 @@
 package com.multimodal.ihm;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.multimodal.ihm.adapter.ViewPagerAdapter;
 import com.multimodal.ihm.fragment.FragmentOne;
 import com.multimodal.ihm.fragment.FragmentThree;
 import com.multimodal.ihm.fragment.FragmentTwo;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
-
-    ViewPager viewPager;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private float xPos, yPos;
+    private float xAccel, yAccel;
+    private float xMax, yMax;
+    private SensorManager sensorManager;
+    private ExecutorService executorService;
+    private Future<View> futureView;
+    private float standingCalibration = 0;
+    private int calibrationCount = 0;
+    private boolean isCalibrated;
+    private boolean activateCursorInteraction = false;
+    private  ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        executorService = new ThreadPoolExecutor(1,1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>());
 
-        viewPager = findViewById(R.id.viewPager);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        addTabs(viewPager);
-        ((TabLayout) findViewById(R.id.tabLayout)).setupWithViewPager( viewPager );
+        Point size = new Point();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        xMax = (float) size.x - 100;
+        yMax = (float) size.y - calculateActionBarHeight() * 2;
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        setupCursor();
+        setupViewPager();
     }
 
-    private void addTabs(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFrag(new FragmentOne(), "ONE");
-        adapter.addFrag(new FragmentTwo(), "TWO");
-        adapter.addFrag(new FragmentThree(), "THREE");
-        viewPager.setAdapter(adapter);
+    private void setupViewPager() {
+        viewPager = findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFrag(new FragmentOne(), "ONE");
+        viewPagerAdapter.addFrag(new FragmentTwo(), "TWO");
+        viewPagerAdapter.addFrag(new FragmentThree(), "THREE");
+        viewPager.setAdapter(viewPagerAdapter);
         viewPager.setOffscreenPageLimit(3);
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        executorService = new ThreadPoolExecutor(1,1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>());
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(futureView != null) {
+            futureView.cancel(true);
+        }
+        executorService.shutdownNow();
+    }
+
+    @Override
+    public boolean onTouchEvent(final MotionEvent event)
+    {
+        /*if(event.getPointerCount() > 2) {
+            activateCursorInteraction = true;
+        }
+        if(activateCursorInteraction){
+            setupCursor();
+        }*/
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode){
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+          //      if(activateCursorInteraction){
+                    onVolumeDownClicked(tabLayout,getCurrentFragment(), xPos, yPos);
+            //    }
+                break;
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                Log.e("main","keyUp");
+                break;
+        }
+        return true;
+    }
+
+    private Fragment getCurrentFragment() {
+        int position = viewPager.getCurrentItem();
+        return viewPagerAdapter.getRegisteredFragment(position);
+    }
+
+    private void onVolumeDownClicked(TabLayout tabLayoutView, Fragment fragmentView, float x, float y) {
+        searchElementInTabLayoutToSwitchPage(tabLayoutView, x, y);
+        futureView = executorService.submit(() -> searchForElementToClick(fragmentView.getView(), x,y));
+        tryGetViewThenClick();
+    }
+
+    private void tryGetViewThenClick() {
+        try {
+            View viewToClick = futureView.get();
+            if(viewToClick != null) {
+                simulatesClick(viewToClick);
+            }
+        } catch (ExecutionException | InterruptedException ignored) {
+        }
+    }
+
+    private void setupCursor(){
+        FrameLayout frameLayout = findViewById(R.id.layout_main);
+        BallView ballView = new BallView(this);
+        frameLayout.addView(ballView);
+    }
+
+    private void simulatesClick(final View viewToClick) {
+        //initiate the button
+        viewToClick.performClick();
+        viewToClick.setPressed(true);
+        viewToClick.invalidate();
+        // delay completion till animation completes
+        //delay button
+        viewToClick.postDelayed(() -> {
+            viewToClick.setPressed(false);
+            viewToClick.invalidate();
+            //any other associated action
+        }, 300);  // .3secs delay time
+    }
+
+    private View searchForElementToClick(View view, float x, float y) {
+        ArrayList<View> allViews = getAllClickableView((ViewGroup) view);
+        for(int i = 0; i < allViews.size();  i++)
+        {
+            View child = allViews.get(i);
+            Rect bounds = new Rect();
+            child.getHitRect(bounds);
+            if( bounds.contains((int) x,(int) y)){
+                return child;
+            }
+        }
+        return null;
+    }
+
+    private void searchElementInTabLayoutToSwitchPage(View view, float x, float y) {
+        Rect mainRect = new Rect();
+        view.getHitRect(mainRect);
+        int childRectWidth = view.getWidth() / 3;
+        Rect rect1 = new Rect(view.getLeft(), view.getTop(), childRectWidth, view.getBottom());
+        Rect rect2 = new Rect(rect1.left + childRectWidth, view.getTop(), rect1.left + childRectWidth*2, view.getBottom());
+        Rect rect3 = new Rect(rect2.left + childRectWidth, view.getTop(), rect2.left + childRectWidth*2, view.getBottom());
+        if(rect1.contains((int) x, (int) y)) {
+            viewPager.setCurrentItem(0);
+        }else if(rect2.contains((int) x, (int) y)){
+            viewPager.setCurrentItem(1);
+        }else if(rect3.contains((int) x, (int) y)){
+            viewPager.setCurrentItem(2);
+        }
+    }
+
+    private ArrayList<View> getAllClickableView(ViewGroup v) {
+        ArrayList<View> clickableViews = new ArrayList<>();
+        for (int i = 0; i < v.getChildCount(); i++) {
+            View child = v.getChildAt(i);
+            if(child instanceof ViewGroup) {
+                clickableViews.addAll(getAllClickableView((ViewGroup) child));
+            }else if(child instanceof TextView || child instanceof ImageView ) { // TODO support all clickable view
+                clickableViews.add(child);
+            }
+        }
+        return clickableViews;
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            xAccel = sensorEvent.values[0] * speedCalibrationX(sensorEvent);
+            yAccel = -(sensorEvent.values[1] - standingCalibration) * speedCalibrationY(sensorEvent);
+            calibrateOrUpdateBall(sensorEvent);
+        }
+    }
+
+
+    private void calibrateOrUpdateBall(SensorEvent sensorEvent){
+        if(!isCalibrated) {
+            calibrationCount +=1;
+            standingCalibration = (calibrationCount * standingCalibration + sensorEvent.values[1]) / (calibrationCount+1);
+            isCalibrated = calibrationCount > 100;
+        }
+        if(isCalibrated) {
+            updateBall();
+        }
+    }
+
+    private float speedCalibrationX(SensorEvent sensorEvent) {
+        if (sensorEvent.values[0] - xAccel > -2 && sensorEvent.values[0] - xAccel < 2) {
+            return 0f; // si il y a une petite orientation => ne rien faire
+        } else if (sensorEvent.values[0] - xAccel > -6 && sensorEvent.values[0] - xAccel < 6) {
+            return 2f;
+        } else {
+            return 6f; // si il y a une grande orientation => multiplier l'effet par 6
+        }
+    }
+
+    private float speedCalibrationY(SensorEvent sensorEvent) {
+        if (sensorEvent.values[1] - yAccel > -1 && sensorEvent.values[1] - yAccel < 1) {
+            return 0f; // si il y a une petite orientation => ne rien faire
+        } else if (sensorEvent.values[1] - xAccel > -6 && sensorEvent.values[1] - xAccel < 6) {
+            return 2f;
+        } else {
+            return 6f; // si il y a une grande orientation => multiplier l'effet par 6 // TODO maybe it's too much?
+        }
+    }
+
+    private void updateBall() {
+
+        xPos -= xAccel;
+        yPos -= yAccel;
+
+        if (xPos > xMax) {
+            xPos = xMax;
+        } else if (xPos < 0) {
+            xPos = 0;
+        }
+
+        if (yPos > yMax) {
+            yPos = yMax;
+        } else if (yPos < 0) {
+            yPos = 0;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    private int calculateActionBarHeight(){
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true))
+        {
+            return TypedValue.complexToDimensionPixelSize(tv.data,getResources().getDisplayMetrics());
+        }
+        return 0;
+    }
+
+    private class BallView extends View {
+        private Bitmap ball;
+
+        public BallView(Context context) {
+            super(context);
+            Bitmap ballSrc = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+            final int dstWidth = 100;
+            final int dstHeight = 100;
+            ball = Bitmap.createScaledBitmap(ballSrc, dstWidth, dstHeight, true);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawBitmap(ball, xPos, yPos, null);
+            invalidate();
+        }
     }
 }
--- app/src/main/java/com/multimodal/ihm/adapter/ViewPagerAdapter.java
+++ app/src/main/java/com/multimodal/ihm/adapter/ViewPagerAdapter.java
@@ -3,6 +3,8 @@ package com.multimodal.ihm.adapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.SparseArray;
+import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.List;
 public class ViewPagerAdapter extends FragmentPagerAdapter {
     private final List<Fragment> mFragmentList = new ArrayList<>();
     private final List<String> mFragmentTitleList = new ArrayList<>();
+    private SparseArray<Fragment> registeredFragments = new SparseArray<>();
 
     public ViewPagerAdapter(FragmentManager manager) {
         super(manager);
@@ -24,6 +27,23 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return mFragmentList.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        Fragment fragment = (Fragment) super.instantiateItem(container, position);
+        registeredFragments.put(position, fragment);
+        return fragment;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        registeredFragments.remove(position);
+        super.destroyItem(container, position, object);
+    }
+
+    public Fragment getRegisteredFragment(int position) {
+        return registeredFragments.get(position);
     }
 
     public void addFrag(Fragment fragment, String title) {
